85d71689636b65392ed4a2b939471238bb583df5
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-04-21 21:10:35 +0800

    Add reset

--- src/App.js
+++ src/App.js
@@ -4,28 +4,33 @@ function App() {
   const [bill, setBill] = useState(0);
   const [service, setService] = useState(0);
   const [serviceFriend, setServiceFriend] = useState(0);
+  const tip = bill * ((service + serviceFriend) / 2 / 100);
+  // function handleBill(value) {
+  //   setBill(() => value);
+  // }
 
-  function handleBill(value) {
-    setBill(() => value);
+  // function handleService(value) {
+  //   console.log(value);
+  //   setService(() => bill / value);
+  // }
+
+  // function handleServiceFriend(value) {
+  //   console.log(value);
+  //   setServiceFriend(() => bill / value);
+  // }
+
+  function handleReset() {
+    setBill("");
+    setService(0);
+    setServiceFriend(0);
   }
-
-  function handleService(value) {
-    console.log(value);
-    setService(() => bill / value);
-  }
-
-  function handleServiceFriend(value) {
-    console.log(value);
-    setServiceFriend(() => bill / value);
-  }
-
   return (
     <>
-      <Bill bill={bill} handleBill={handleBill} />
-      <Service handleService={handleService} />
-      <ServiceForFried handleServiceFriend={handleServiceFriend} />
-      <TotalBill serviceFriend={serviceFriend} service={service} bill={bill} />
-      <ResetBill />
+      <Bill bill={bill} handleBill={setBill} />
+      <Service handleService={setService} />
+      <ServiceForFried handleServiceFriend={setServiceFriend} />
+      <TotalBill bill={bill} tip={tip} />
+      <ResetBill onReset={handleReset} />
     </>
   );
 }
@@ -69,17 +74,16 @@ function ServiceForFried({ handleServiceFriend }) {
   );
 }
 
-function TotalBill({ service, bill, serviceFriend }) {
+function TotalBill({ bill, tip }) {
   return (
     <h2>
-      You pay ${Number(bill) + Number(service + serviceFriend / 2)} (${bill} + $
-      {service + serviceFriend / 2} tip)
+      You pay ${Number(bill) + Number(tip)} (${bill} + ${tip} tip)
     </h2>
   );
 }
 
-function ResetBill() {
-  return <button>Reset</button>;
+function ResetBill({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
 
 export default App;
