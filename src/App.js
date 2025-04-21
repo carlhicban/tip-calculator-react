ce486ef92e581c75dcc9d3d9b5bedb972f17e472
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-04-21 20:38:09 +0800

    Add service for percent

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 function App() {
   const [bill, setBill] = useState(0);
+  const [service, setService] = useState(0);
+  const [serviceFriend, setServiceFriend] = useState(0);
 
   function handleBill(value) {
     setBill(() => value);
   }
+
+  function handleService(value) {
+    console.log(value);
+    setService(() => bill / value);
+  }
+
+  function handleServiceFriend(value) {
+    console.log(value);
+    setServiceFriend(() => bill / value);
+  }
+
   return (
     <>
       <Bill bill={bill} handleBill={handleBill} />
-      <Service />
-      <TotalBill bill={bill} />
+      <Service handleService={handleService} />
+      <ServiceForFried handleServiceFriend={handleServiceFriend} />
+      <TotalBill serviceFriend={serviceFriend} service={service} bill={bill} />
       <ResetBill />
     </>
   );
@@ -29,17 +43,39 @@ function Bill({ bill, handleBill }) {
   );
 }
 
-function Service() {
+function Service({ handleService }) {
   return (
     <>
       <h3>How did you like the service?</h3>
-      <input type="number" />
+      <select onChange={(e) => handleService(Number(e.target.value))}>
+        <option value={0}>0%</option>
+        <option value={10}>10%</option>
+        <option value={20}>20%</option>
+      </select>
     </>
   );
 }
 
-function TotalBill({ bill }) {
-  return <h2>You pay $ (${bill} + $ tip)</h2>;
+function ServiceForFried({ handleServiceFriend }) {
+  return (
+    <>
+      <h3>How did your friend like the service?</h3>
+      <select onChange={(e) => handleServiceFriend(Number(e.target.value))}>
+        <option value={0}>0%</option>
+        <option value={10}>10%</option>
+        <option value={20}>20%</option>
+      </select>
+    </>
+  );
+}
+
+function TotalBill({ service, bill, serviceFriend }) {
+  return (
+    <h2>
+      You pay ${Number(bill) + Number(service + serviceFriend / 2)} (${bill} + $
+      {service + serviceFriend / 2} tip)
+    </h2>
+  );
 }
 
 function ResetBill() {
